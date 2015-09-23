5cb2a31a053ae41d3211665013d573aa295bce8f
mattbrun <me@matteobrunati.net>
2015-09-23 02:07:57 +0200

    Added tag cloud to front page

--- wp-content/themes/tiles/index.php
+++ wp-content/themes/tiles/index.php
@@ -1,6 +1,28 @@
 <?php get_header(); ?>
 
 <div id="contentwrapper">
+  <div class="home-tags-cloud" style="width:300px;height:75px;margin-left:auto;margin-right:auto;">
+    <!--<h1>Tags:</h1>-->
+    <?php show_tag_cloud(array(
+      taxomony=>"post_tag",
+      /*
+      number=>"0",
+      order_by=>"count",
+      order=>"RAND",
+      format=>"list",
+      opacity=>"1",
+      tilt=>"1",
+      colorize=>"1",
+      cache=>"1",
+      timeout=>"300",
+      zoom=>"1",
+      smallest=>"75",
+      largest=>"200",
+      color=>"#FF5800",
+      background=>"FFFFFF",
+      */
+    ));?>
+  </div>
   <div class="grid-wrap">
     <ul class="grid swipe-right" id="grid">
       <?php query_posts( 'posts_per_page=-1' ); ?>
