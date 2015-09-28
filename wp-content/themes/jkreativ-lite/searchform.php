4f48b28cca940b12024d272926e525bd90244eef
mattbrun <me@matteobrunati.net>
2015-09-28 17:36:43 +0200

    Removed unused themes

--- wp-content/themes/jkreativ-lite/searchform.php
+++
@@ -1,3 +0,0 @@
-<form method="get" action="<?php echo trailingslashit(home_url()); ?>">
-	<input type="text" autocomplete="off" name="s" placeholder="<?php _e('Type and Enter to Search', 'jkreativ-lite'); ?>">
-</form>
--- wp-content/themes/jkreativ-lite/template/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="mainsidebar">
-	<div class="mainsidebar-wrapper">
-		<?php
-			if(is_active_sidebar('sidebar-main')) {
-				dynamic_sidebar('sidebar-main');
-			}
-		?>
-	</div>
-</div>
